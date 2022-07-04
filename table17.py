ee9fa624f813098dead4c1909eb0e913e0edb309
shekhar suman <shekharpre@gmail.com>
2022-07-04 15:49:32 +0530

    table17

---
+++ table17.py
@@ -0,0 +1,8 @@
+
+#***Table Counting from 1 to 10***
+num = 17
+#To take input from the user
+#num = int(input("Display multiplication table of ..."))
+#Iterate 10 times from i = 1 to 10
+for i in range(1,11):
+    print(num, 'X', i, '=', num*i)
